ed9f998c1386b770127c752ea14ea885634555de
Fuad Talukder <webfuadtk@gmail.com>
2024-11-23 10:49:39 +0600

    fixed font issue

---
+++ src/app/fonts/fonts.jsx
@@ -0,0 +1,12 @@
+import { Manrope, Pacifico } from "next/font/google";
+
+export const manrope = Manrope({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const pacifico = Pacifico({
+  subsets: ["latin"],
+  display: "swap",
+  weight: "400",
+});
--- src/components/NewCustomerOffer/NewCustomerOffer.jsx
+++ src/components/NewCustomerOffer/NewCustomerOffer.jsx
@@ -3,13 +3,16 @@ import Container from "../utils/Container";
 import ShopBtn from "../utils/CommonBtn/ShopBtn";
 import Image from "next/image";
 import offerImg from "../../assets/images/offerImg.png";
+import { pacifico } from "../../app/fonts/fonts.jsx";
 
 const NewCustomerOffer = () => {
   return (
     <Container>
       <div className="flex items-center justify-between bg-[#F4F8FF] px-10 mt-5">
         <div>
-          <p className="text-purple-600 text-2xl">Big Deal</p>
+          <p className={`text-purple-600 text-2xl ${pacifico.className}`}>
+            Big Deal
+          </p>
           <p className="text-purple-600 text-5xl font-semibold mt-2">
             30% <span className="text-black">Off for New Customers</span>
           </p>
